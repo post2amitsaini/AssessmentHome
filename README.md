c1a440c062f3ebb047c6f48dcc5de19974e74349
Amit Saini <post2amitsaini@gmail.com>
2023-08-31 19:40:10 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AssessmentHome
+
+A description of this package.
---
+++ Sources/AssessmentHome/AssessmentHome.swift
@@ -0,0 +1,6 @@
+public struct AssessmentHome {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
